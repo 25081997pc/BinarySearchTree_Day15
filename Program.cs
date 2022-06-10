32be33ddafc32de063c1da69b19949d1632b65ed
25081997pc <chavanpooja2508@gmail.com>
2022-06-10 15:31:32 +0530

    messege

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+namespace BinarySearchTree_Day15
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            binarySearch.GetSize();
+        }
+    }
+}
