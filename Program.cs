af51ea119ee097a56d7efd623efbe3844d7fc436
25081997pc <chavanpooja2508@gmail.com>
2022-06-10 17:36:06 +0530

    Inserting Values

--- Program.cs
+++ Program.cs
@@ -11,8 +11,21 @@ namespace BinarySearchTree_Day15
 
             binarySearch.Insert(30);
             binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
+
             binarySearch.Display();
             binarySearch.GetSize();
+            
         }
     }
+
 }
